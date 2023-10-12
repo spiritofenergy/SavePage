50be5c5419c7dde9996bdddc86ceab9360cb9ceb
Spirit of energy <spiritofenergy220@gmail.com>
2023-10-12 07:59:42 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,17 @@ import java.net.URLConnection;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        int index = 0;
+        for (int i = 0; i < 50; i++){
+            String sourceCode = downloadWebPage("https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0");
+            String name = "Random.html";
+            index++;
+            writeToFile(sourceCode, name, index);
 
-        String sourceCode17 = downloadWebPage("https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%BE%D0%B1%D0%BB%D0%B5%D0%BC%D0%B0_2000_%D0%B3%D0%BE%D0%B4%D0%B0");
-        String sourceCode18 = downloadWebPage("https://ru.wikipedia.org/wiki/%D0%9A%D0%BD%D1%83%D1%82,_%D0%94%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%B4_%D0%AD%D1%80%D0%B2%D0%B8%D0%BD");
 
-        writeToFile17(sourceCode17);
-        writeToFile18(sourceCode18.replaceAll("Кнут" , " Пряник"));
+        }
+
+
     }
     private static String downloadWebPage(String url) throws IOException {
         StringBuilder result = new StringBuilder();
@@ -28,15 +33,15 @@ public class Main {
         return result.toString();
 
     }
-    public static void writeToFile17(String str) throws Exception {
-        BufferedWriter writer17 = new BufferedWriter(new FileWriter("C:\\Users\\Marat\\Desktop\\test\\2000.html"));
-        writer17.write(str);
-        writer17.close();
-    }
-        public static void writeToFile18(String str) throws Exception{
-        BufferedWriter writer18 = new BufferedWriter(new FileWriter("C:\\Users\\Marat\\Desktop\\test\\Knut.html"));
-        writer18.write(str);
-        writer18.close();
+    public static void writeToFile(String str, String name, int index) throws Exception {
+
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Marat\\Desktop\\test\\" + index + " " + name ));
+
+        System.out.println(index + name);
+        writer.write(str);
+        writer.close();
+
     }
 }
 
